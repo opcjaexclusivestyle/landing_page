b0ef62f188710b9c2d76626611e75a06d9861dd3
Konrad Gruca <konrad.gruca90@gmail.com>
2025-04-11 18:09:40 +0200

    fix

---
+++ app/blog/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+export const metadata = {
+  title: 'Blog - Inspiracje wnętrzarskie',
+  description:
+    'Odkryj najnowsze trendy wnętrzarskie, porady ekspertów i inspirujące historie',
+};
+
+export default function BlogLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className='blog-layout'>
+      {/* Możesz dodać tutaj nagłówek bloga, menu kategorii itp. */}
+      <main>{children}</main>
+      {/* Możesz dodać tutaj stopkę specyficzną dla bloga */}
+    </div>
+  );
+}
